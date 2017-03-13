f24106fd12ec498a785eda31c62526b4fc89075f
Alec Maliwanag <amaliwanag@gmail.com>
2017-03-13 15:55:18 -0700

    linted

--- StockProblem.js
+++ StockProblem.js
@@ -1,15 +1,15 @@
 
-var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+const stockPricesYesterday = [10, 7, 5, 8, 11, 9];
 
-var getMaxProfit = function(values) {
-  var min = values[0];
-  var maxProfit = 0;
-  for (var i = 0; i < values.length; i++) {
-    if(values[i] < min) min = values[i];
-    if(values[i] - min > maxProfit) maxProfit = values[i] - min;
+const getMaxProfit = function test(values) {
+  let min = values[0];
+  let maxProfit = 0;
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] < min) min = values[i];
+    if (values[i] - min > maxProfit) maxProfit = values[i] - min;
   }
   return maxProfit;
 };
 
 getMaxProfit(stockPricesYesterday);
-// returns 6 (buying for $5 and selling for $11)i
+// returns 6 (buying for $5 and selling for $11)

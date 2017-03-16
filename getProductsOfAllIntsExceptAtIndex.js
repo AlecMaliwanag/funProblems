a5514cf7efdcbdf9cfe5d7b16652d52aef6569ac
Alec Maliwanag <amaliwanag@gmail.com>
2017-03-16 09:43:15 -0700

    Implement in O of N time with if statements

--- getProductsOfAllIntsExceptAtIndex.js
+++ getProductsOfAllIntsExceptAtIndex.js
@@ -1,17 +1,24 @@
 const sampleArray = [1, 7, 3, 4];
 const getProductsofAllIntsExceptAtIndex = function products(array) {
   //iterate over each, nested for loop
-  let results = [];
-  array.forEach((val, index, arg) => {
-    let sum = 1;
-    for (let i = 0; i < arg.length; i++) {
-      if (i !== index) {
-        sum = sum * arg[i];
-      }
-    }
-    results.push(sum);
-  });
-  return results;
+//   let results = [];
+//   array.forEach((val, index, arg) => {
+//     let sum = 1;
+//     for (let i = 0; i < arg.length; i++) {
+//       if (i !== index) {
+//         sum = sum * arg[i];
+//       }
+//     }
+//     results.push(sum);
+//   });
+//   return results;
+// };
+  const productsOfAllIntsBeforeIndex = [];
+  let productSofar = 1;
+  for (let i = 0; i < array.length; i+=1) {
+    productsOfAllIntsBeforeIndex[i] = productSofar;
+    productSofar *= array[i];
+  }
 };
 
 getProductsofAllIntsExceptAtIndex(sampleArray);
